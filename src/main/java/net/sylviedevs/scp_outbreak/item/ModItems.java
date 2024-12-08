38b90a91003b6a8f9039426376fc1baadf34fdd1
Cris <cristianhostert@gmail.com>
2024-12-08 03:55:13 -0300

    Changed Packages

--- src/main/java/net/sylviedevs/scp_outbreak/item/ModItems.java
+++ src/main/java/net/sylviedevs/scp_outbreak/item/ModItems.java
@@ -1,10 +1,29 @@
 package net.sylviedevs.scp_outbreak.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import net.sylviedevs.scp_outbreak.SCPOutbreak;
 
 public class ModItems {
+    public static final Item SCP_109 = registerItem("scp_109", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(SCP_109);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(SCPOutbreak.MOD_ID, name), item);
+    }
 
     public static void registerModItems() {
-        SCPOutbreak.LOGGER.info("[" + SCPOutbreak.MOD_ID + "] Registering Mod Items");
+        SCPOutbreak.LOGGER.info("Registering Items...");
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
     }
 }
