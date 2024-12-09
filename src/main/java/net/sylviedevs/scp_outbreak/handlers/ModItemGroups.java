381a9e5f3583f03536b0ceefbed0cadd9743ff09
Cris <cristianhostert@gmail.com>
2024-12-09 04:21:53 -0300

    Made Item Groups work more 'modular'

--- src/main/java/net/sylviedevs/scp_outbreak/handlers/ModItemGroups.java
+++ src/main/java/net/sylviedevs/scp_outbreak/handlers/ModItemGroups.java
@@ -12,11 +12,19 @@ import net.minecraft.util.Identifier;
 public class ModItemGroups {
     public static final ItemGroup OBJECTS_GROUP = Registry.register(Registries.ITEM_GROUP,
             new Identifier(SCPOutbreak.MOD_ID, "objects"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup." + SCPOutbreak.MOD_ID + ".objects"))
-                    .icon(() -> new ItemStack( ModItems.retrieveItemFromIdentifier("scp_109") )).entries((displayContext, entries) -> {
-                        entries.add( ModItems.retrieveItemFromIdentifier("scp_109") );
-                    }).build());
+            FabricItemGroup.builder()
+                    .displayName(Text.translatable("itemgroup." + SCPOutbreak.MOD_ID + ".objects"))
+                    .icon(() -> new ItemStack( ModItems.retrieveItemFromIdentifier("scp_109") ))
+                    .entries((displayContext, entries) -> setItemsInGroup("objects", entries))
+                    .build());
 
+    public static void setItemsInGroup(String tabName, ItemGroup.Entries entries) {
+        for (String[] thisItemIdentifier : ModItems.itemIdentifiers) {
+            if ( !tabName.equals(thisItemIdentifier[1]) ) { continue; }
+
+            entries.add( ModItems.retrieveItemFromIdentifier(thisItemIdentifier[0]) );
+        }
+    }
 
     public static void registerItemGroups() {
         SCPOutbreak.LOGGER.info("Registering Item Groups...");
